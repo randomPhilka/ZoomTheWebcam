8e5e8896a0ceb4770e0e93dbec49581a1c0ca5d3
Philip Boyko <p.boyko@bankingon.io>
2022-11-06 15:03:15 +0400

    initial commit

---
+++ WebCam/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WebCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this._zoomInButton = new System.Windows.Forms.Button();
+            this._zoomOutButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(248, 129);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(6);
+            this.groupBox1.Size = new System.Drawing.Size(1228, 783);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1228, 772);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // _zoomInButton
+            // 
+            this._zoomInButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this._zoomInButton.Location = new System.Drawing.Point(1164, 73);
+            this._zoomInButton.Margin = new System.Windows.Forms.Padding(6);
+            this._zoomInButton.Name = "_zoomInButton";
+            this._zoomInButton.Size = new System.Drawing.Size(150, 44);
+            this._zoomInButton.TabIndex = 1;
+            this._zoomInButton.Text = "Zoom In";
+            this._zoomInButton.UseVisualStyleBackColor = true;
+            this._zoomInButton.Click += new System.EventHandler(this._zoomInButton_Click);
+            // 
+            // _zoomOutButton
+            // 
+            this._zoomOutButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this._zoomOutButton.Location = new System.Drawing.Point(1326, 73);
+            this._zoomOutButton.Margin = new System.Windows.Forms.Padding(6);
+            this._zoomOutButton.Name = "_zoomOutButton";
+            this._zoomOutButton.Size = new System.Drawing.Size(150, 44);
+            this._zoomOutButton.TabIndex = 1;
+            this._zoomOutButton.Text = "Zoom Out";
+            this._zoomOutButton.UseVisualStyleBackColor = true;
+            this._zoomOutButton.Click += new System.EventHandler(this._zoomOutButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(603, 956);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(500, 33);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(603, 1010);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(500, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Srart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1266, 956);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1752, 1097);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this._zoomOutButton);
+            this.Controls.Add(this._zoomInButton);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button _zoomInButton;
+        private System.Windows.Forms.Button _zoomOutButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WebCam/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+using System.IO.Ports;
+using System.Threading;
+
+using System.Runtime.InteropServices;
+
+namespace WebCam
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection VideoCaptureDevices;
+        private VideoCaptureDevice FinalVideo;
+
+        static SerialPort _serialPort;
+
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            //First
+            topFirstRange = (int)(pictureBox1.Top - (pictureBox1.Height * 0.1));
+            leftFirstRange = (int)(pictureBox1.Left - (pictureBox1.Width * 0.1));
+            heightFirstRange = (int)(pictureBox1.Height + (pictureBox1.Height * 0.15));
+            widthFirstRange = (int)(pictureBox1.Width + (pictureBox1.Width * 0.15));
+            //Thecond
+            topSecondRange = (int)(pictureBox1.Top - (pictureBox1.Height * 0.2));
+            leftSecondRange = (int)(pictureBox1.Left - (pictureBox1.Width * 0.2));
+            heightSecondRange = (int)(pictureBox1.Height + (pictureBox1.Height * 0.25));
+            widthSecondRange = (int)(pictureBox1.Width + (pictureBox1.Width * 0.25));
+            //Third
+            topThirdRange = (int)(pictureBox1.Top - (pictureBox1.Height * 0.3));
+            leftThirdRange = (int)(pictureBox1.Left - (pictureBox1.Width * 0.3));
+            heightThirdRange = (int)(pictureBox1.Height + (pictureBox1.Height * 0.35));
+            widthThirdRange = (int)(pictureBox1.Width + (pictureBox1.Width * 0.35));
+            //Fourth
+            topFourthRange = (int)(pictureBox1.Top - (pictureBox1.Height * 0.4));
+            leftFourthRange = (int)(pictureBox1.Left - (pictureBox1.Width * 0.4));
+            heightFourthRange = (int)(pictureBox1.Height + (pictureBox1.Height * 0.45));
+            widthFourthRange = (int)(pictureBox1.Width + (pictureBox1.Width * 0.45));
+            //Fifth
+            topFifthRange = (int)(pictureBox1.Top - (pictureBox1.Height * 0.5));
+            leftFifthRange = (int)(pictureBox1.Left - (pictureBox1.Width * 0.5));
+            heightFifthRange = (int)(pictureBox1.Height + (pictureBox1.Height * 0.55));
+            widthFifthRange = (int)(pictureBox1.Width + (pictureBox1.Width * 0.55));
+            //Sixth
+            topSixthRange = (int)(pictureBox1.Top - (pictureBox1.Height * 0.6));
+            leftSixthRange = (int)(pictureBox1.Left - (pictureBox1.Width * 0.6));
+            heightSixthRange = (int)(pictureBox1.Height + (pictureBox1.Height * 0.65));
+            widthSixthRange = (int)(pictureBox1.Width + (pictureBox1.Width * 0.65));
+            //Seventh
+            topSeventhRange = (int)(pictureBox1.Top - (pictureBox1.Height * 0.7));
+            leftSeventhRange = (int)(pictureBox1.Left - (pictureBox1.Width * 0.7));
+            heightSeventhRange = (int)(pictureBox1.Height + (pictureBox1.Height * 0.75));
+            widthSeventhRange = (int)(pictureBox1.Width + (pictureBox1.Width * 0.75));
+            //Eighth
+            topEighthRange = (int)(pictureBox1.Top - (pictureBox1.Height * 0.8));
+            leftEighthRange = (int)(pictureBox1.Left - (pictureBox1.Width * 0.8));
+            heightEighthRange = (int)(pictureBox1.Height + (pictureBox1.Height * 0.85));
+            widthEighthRange = (int)(pictureBox1.Width + (pictureBox1.Width * 0.85));
+            //Ninth
+            topNinthRange = (int)(pictureBox1.Top - (pictureBox1.Height * 0.9));
+            leftNinthRange = (int)(pictureBox1.Left - (pictureBox1.Width * 0.9));
+            heightNinthRange = (int)(pictureBox1.Height + (pictureBox1.Height * 0.95));
+            widthNinthRange = (int)(pictureBox1.Width + (pictureBox1.Width * 0.95));
+            //Tenth
+            topTenthRange = (int)(pictureBox1.Top - (pictureBox1.Height * 1));
+            leftTenthRange = (int)(pictureBox1.Left - (pictureBox1.Width * 1));
+            heightTenthRange = (int)(pictureBox1.Height + (pictureBox1.Height * 1.5));
+            widthTenthRange = (int)(pictureBox1.Width + (pictureBox1.Width * 1.5));
+
+
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+
+            if (_serialPort == null)
+            {
+                _serialPort = new SerialPort();
+                _serialPort.PortName = "COM3";
+                _serialPort.BaudRate = 9600;
+                _serialPort.Open();
+            }
+
+            int form = (int)(pictureBox1.Top - (pictureBox1.Height * 0.025));
+
+
+        }
+
+        //First
+        int topFirstRange;
+        int leftFirstRange;
+        int heightFirstRange;
+        int widthFirstRange;
+        //Second
+        int topSecondRange;
+        int leftSecondRange;
+        int heightSecondRange;
+        int widthSecondRange;
+        //Third
+        int topThirdRange;
+        int leftThirdRange;
+        int heightThirdRange;
+        int widthThirdRange;
+        //Fourth
+        int topFourthRange;
+        int leftFourthRange;
+        int heightFourthRange;
+        int widthFourthRange;
+        //Fifth
+        int topFifthRange;
+        int leftFifthRange;
+        int heightFifthRange;
+        int widthFifthRange;
+        //Sixth
+        int topSixthRange;
+        int leftSixthRange;
+        int heightSixthRange;
+        int widthSixthRange;
+        //Seventh
+        int topSeventhRange;
+        int leftSeventhRange;
+        int heightSeventhRange;
+        int widthSeventhRange;
+        //Eighth
+        int topEighthRange;
+        int leftEighthRange;
+        int heightEighthRange;
+        int widthEighthRange;
+        //Ninth
+        int topNinthRange;
+        int leftNinthRange;
+        int heightNinthRange;
+        int widthNinthRange;
+        //Tenth
+        int topTenthRange;
+        int leftTenthRange;
+        int heightTenthRange;
+        int widthTenthRange;
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            _serialPort.NewLine = "\n";
+            string test3 = _serialPort.ReadExisting();
+            string y;
+            int SlashPos = test3.IndexOf("\n");
+
+            if (SlashPos > 0)
+                y = test3.Substring(0, SlashPos);
+            else
+                y = test3;
+
+            label1.Text = y;
+
+         
+            if (Enumerable.Range(0, 10).Contains(Convert.ToInt32(y)))
+            {
+                    pictureBox1.Top = topFirstRange;
+                    pictureBox1.Left = leftFirstRange;
+                    pictureBox1.Height = heightFirstRange;
+                    pictureBox1.Width = widthFirstRange;
+            }
+            else if (Enumerable.Range(11, 60).Contains(Convert.ToInt32(y)))
+            {
+                    pictureBox1.Top = topSecondRange;
+                    pictureBox1.Left = leftSecondRange;
+                    pictureBox1.Height = heightSecondRange;
+                    pictureBox1.Width = widthSecondRange;
+            }
+            else if (Enumerable.Range(61, 110).Contains(Convert.ToInt32(y)))
+            {
+                pictureBox1.Top = topThirdRange;
+                pictureBox1.Left = leftThirdRange;
+                pictureBox1.Height = heightThirdRange;
+                pictureBox1.Width = widthThirdRange;
+            }
+            else if (Enumerable.Range(111, 170).Contains(Convert.ToInt32(y)))
+            {
+                pictureBox1.Top = topFourthRange;
+                pictureBox1.Left = leftFourthRange;
+                pictureBox1.Height = heightFourthRange;
+                pictureBox1.Width = widthFourthRange;
+            }
+            else if (Enumerable.Range(171, 230).Contains(Convert.ToInt32(y)))
+            {
+                pictureBox1.Top = topFifthRange;
+                pictureBox1.Left = leftFifthRange;
+                pictureBox1.Height = heightFifthRange;
+                pictureBox1.Width = widthFifthRange;
+            }
+            else if (Enumerable.Range(231, 350).Contains(Convert.ToInt32(y)))
+            {
+                pictureBox1.Top = topSixthRange;
+                pictureBox1.Left = leftSixthRange;
+                pictureBox1.Height = heightSixthRange;
+                pictureBox1.Width = widthSixthRange;
+            }
+            else if (Enumerable.Range(351, 500).Contains(Convert.ToInt32(y)))
+            {
+                pictureBox1.Top = topSeventhRange;
+                pictureBox1.Left = leftSeventhRange;
+                pictureBox1.Height = heightSeventhRange;
+                pictureBox1.Width = widthSeventhRange;
+            }
+            else if (Enumerable.Range(501, 650).Contains(Convert.ToInt32(y)))
+            {
+                pictureBox1.Top = topEighthRange;
+                pictureBox1.Left = leftEighthRange;
+                pictureBox1.Height = heightEighthRange;
+                pictureBox1.Width = widthEighthRange;
+            }
+            else if (Enumerable.Range(651, 750).Contains(Convert.ToInt32(y)))
+            {
+                pictureBox1.Top = topNinthRange;
+                pictureBox1.Left = leftNinthRange;
+                pictureBox1.Height = heightNinthRange;
+                pictureBox1.Width = widthNinthRange;
+            }
+            else if (Enumerable.Range(751, 1024).Contains(Convert.ToInt32(y)))
+            {
+                pictureBox1.Top = topTenthRange;
+                pictureBox1.Left = leftTenthRange;
+                pictureBox1.Height = heightTenthRange;
+                pictureBox1.Width = widthTenthRange;
+            }
+        }
+
+
+        void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (_serialPort != null && _serialPort.IsOpen)
+            {
+                _serialPort.Close();
+            }
+        }
+
+        private void _zoomInButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Top = (int)(pictureBox1.Top - (pictureBox1.Height * 0.025));
+            pictureBox1.Left = (int)(pictureBox1.Left - (pictureBox1.Width * 0.025));
+            pictureBox1.Height = (int)(pictureBox1.Height + (pictureBox1.Height * 0.05));
+            pictureBox1.Width = (int)(pictureBox1.Width + (pictureBox1.Width * 0.05));
+
+        }
+
+        private void _zoomOutButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Top = (int)(pictureBox1.Top + (pictureBox1.Height * 0.025));
+            pictureBox1.Left = (int)(pictureBox1.Left + (pictureBox1.Width * 0.025));
+            pictureBox1.Height = (int)(pictureBox1.Height - (pictureBox1.Height * 0.05));
+            pictureBox1.Width = (int)(pictureBox1.Width - (pictureBox1.Width * 0.05));
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo VideoCaptureDevice in VideoCaptureDevices)
+            {
+                comboBox1.Items.Add(VideoCaptureDevice.Name);
+            }
+
+            comboBox1.SelectedIndex = 0;
+            FinalVideo = new VideoCaptureDevice();
+
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (FinalVideo.IsRunning == true) FinalVideo.Stop();
+
+            FinalVideo = new VideoCaptureDevice(VideoCaptureDevices[comboBox1.SelectedIndex].MonikerString);
+            FinalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+            FinalVideo.Start();
+        }
+
+        void FinalVideo_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap video = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = video;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (FinalVideo.IsRunning == true) FinalVideo.Stop();
+        }
+    }
+}
